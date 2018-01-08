8e6b3ce838a58cc746316d816501a2543d5fb11d
ruano23 <carlosruano23@gmail.com>
2018-01-09 00:54:56 +0100

    add: Añadido el widget de timeline de Twitter-widget
    He añadido el widget de twitter para su timeline de cualquier usuario.
    Sólo habría que corregir que lo muestre una vez y no 2 como lo hace actualmente.
    
    Issue: #24

---
+++ socialhub-egc/widgets/class-timeline-Twitter-widget.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Timeline_Twitter_Widget
+ *
+ * This widget implements the timeline for a user social network.
+ *
+ * @see WP_Widget
+ */
+class Timeline_Twitter_Widget extends WP_Widget {
+	const BASE_ID = 'timeline-Twitter-egc';
+
+	/**
+	 * Register widget with WordPress
+	 *
+	 */
+	public function __construct() {
+		// Instantiate the parent object
+		parent::__construct(static::getBaseID(),
+							'Timeline Twitter by EGC',
+							array('description' => static::getDescription()));
+	}
+
+	/**
+	 * Get the base ID used to identify widgets of this type installed in a widget area
+	 *
+	 * @return string Widget base ID
+	 */
+	public static function getBaseID() {
+		return static::BASE_ID;
+	}
+
+	/**
+	 * Describe the functionality offered by the widget
+	 *
+	 * @return string Description of the widget functionality
+	 */
+	public static function getDescription() {
+		return 'This widget implements the timeline for a user social network.';
+	}
+
+	/**
+	 * Front-end display of widget
+	 *
+	 * @param array $args     Display arguments including before_title, after_title, before_widget, and after_widget
+	 * @param array $instance The settings for the particular instance of the widget
+	 *
+	 * @return void
+	 */
+	public function widget($args, $instance) {
+		// Widget output
+		
+		//TWITTER
+		$user = getUsernameTwitter($instance, 'twitter');	
+		if ($user!="")		
+			echo '<a class="twitter-timeline" data-lang="en" data-theme="dark" 
+				data-width="350" data-height="350"
+				data-link-color="#19CF86"
+				href="https://twitter.com/'.$user.'?ref_src=twsrc%5Etfw">
+				Tweets by '.$user.'
+				<script async src="https://platform.twitter.com/widgets.js" 
+				charset="utf-8">
+				</script>';
+	}
+
+	/**
+	 * Update a widget instance
+	 *
+	 * @param array $new_instance New settings for this instance as input by the user via form()
+	 * @param array $old_instance Old settings for this instance
+	 *
+	 * @return bool|array settings to save or false to cancel saving
+	 */
+	public function update($new_instance, $old_instance) {
+		// Save widget options
+		$instance = $old_instance;
+		$instance['userTwitter'] = strip_tags($new_instance['userTwitter']);
+		$instance = $new_instance;
+		//$instance[''] = strip_tags($new_instance['']); // Strips a string from HTML, XML, and PHP tags
+
+		return $instance;
+	}
+
+	/**
+	 * Settings update form
+	 *
+	 * @param array $instance Current settings
+	 *
+	 * @return void
+	 */
+	public function form($instance) {
+		// Output admin widget options form
+		
+		//Getting usernames
+		$userTwitter = esc_attr($instance['userTwitter']);
+		
+		?>
+			<br/><span id="info">Please, input the account whose information will be shown in the timeline</span>
+			<p>
+				<label for="<?php echo $this->get_field_id('userTwitter'); ?>">
+					<?php _e('Twitter account:'); ?> 
+					<input class="widefat" id="<?php echo $this->get_field_id('userTwitter'); ?>" placeholder="TwitterUser" name="<?php echo $this->get_field_name('userTwitter'); ?>" type="text" value="<?php echo $userTwitter; ?>" />
+				</label>
+			</p>			
+			
+        <?php 
+	}
+}	
+	//AUXILIARY METHODS
+
+/* params: 
+$instance: $instance
+$socialnetwork: twitter */
+function getUsernameTwitter($instance, $socialnetwork){
+	
+			$network = ucfirst($socialnetwork);
+	
+			$user = apply_filters('widget_user'.$network, $instance['user'.$network]);
+	
+	
+			return $user;
+	
+}
+
+
+
+?>
