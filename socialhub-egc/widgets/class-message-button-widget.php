2fdf91579d436d23095086e50f80ceaa04627585
Luis Miguel <Siulcava7@gmail.com>
2018-01-10 17:07:17 +0100

    add: Añadida funcionalidad mensaje por defecto
    Se ha añadido un formulario para introducir un mensaje predefinido al
    widget, ademas se añade una funcion estatica que permite formatear el
    mensaje para que el plugin lo muestre de manera correcta.
    
    Issue: #30

--- socialhub-egc/widgets/class-message-button-widget.php
+++ socialhub-egc/widgets/class-message-button-widget.php
@@ -41,7 +41,8 @@ class Message_Button_Widget extends WP_Widget {
 	/**
 	 * Front-end display of widget
 	 *
-	 * @param array $args     Display arguments including before_title, after_title, before_widget, and after_widget
+	 * @param array $args     Display arguments including before_title, after_title, before_widget, and 
+	 after_widget
 	 * @param array $instance The settings for the particular instance of the widget
 	 *
 	 * @return void
@@ -52,9 +53,15 @@ class Message_Button_Widget extends WP_Widget {
 		//TWITTER
 		$userTwitter = esc_attr($instance['userTwitter']);	
 		$userTwitterID = esc_attr($instance['userTwitterID']);
+		$message = esc_attr($instance['message']);
+		
+		$messageFormateado = static::getMessageFormateado($instance,$message);
 		
 		if ($userTwitter!="")
-		$html .= '<div><a lang="en" class="twitter-dm-button"  href="https://twitter.com/messages/compose?recipient_id='.$userTwitterID.'" data-screen-name="'.$userTwitter.'" data-show-screen-name="false" data-size="large" data-show-count="false"> Message '.userTwitter.'</a></div>';
+			$html .= '<div><a lang="en" class="twitter-dm-button"  href="
+			https://twitter.com/messages/compose?recipient_id='.$userTwitterID.'&text='.messageFormateado.
+			'" data-screen-name="'.$userTwitter.'" data-show-screen-name="false" data-size="large" 
+			data-show-count="false"> Message '.userTwitter.'</a></div>';
 		
 		echo $html;
 	}
@@ -72,6 +79,7 @@ class Message_Button_Widget extends WP_Widget {
 		$instance = $old_instance;
 		$instance['userTwitter'] = strip_tags($new_instance['userTwitter']);
 		$instance['userTwitterID'] = strip_tags($new_instance['userTwitterID']);
+		$instance['message'] = strip_tags($new_instance['message']);
 		//$instance[''] = strip_tags($new_instance['']); // Strips a string from HTML, XML, and PHP tags
 
 		return $instance;
@@ -88,34 +96,47 @@ class Message_Button_Widget extends WP_Widget {
 		$userTwitter = esc_attr($instance['userTwitter']);
 		$userTwitterID = esc_attr($instance['userTwitterID']);
 		?>
-		<br/><span id="info">Please, input the user who will receive de message. Remember the account that you will be used, should have permissions to receive message from unknown people. </span>
+		<br/><span id="info">Please, input the user who will receive de message. Remember the account that
+		you will be used, should have permissions to receive message from unknown people. </span>
 			<p>
 				<label for="<?php echo $this->get_field_id('userTwitter'); ?>">
 					<?php _e('Twitter account to send direct message:'); ?> 
-					<input class="widefat" id="<?php echo $this->get_field_id('userTwitter'); ?>" placeholder="@TwitterUser" name="<?php echo $this->get_field_name('userTwitter'); ?>" type="text" value="<?php echo $userTwitter; ?>" />
+					<input class="widefat" id="<?php echo $this->get_field_id('userTwitter'); ?>" 
+					placeholder="@TwitterUser" name="<?php echo $this->get_field_name('userTwitter'); ?>" 
+					type="text" value="<?php echo $userTwitter; ?>" />
 				</label>
 			</p>
 			<p>
 				<label for="<?php echo $this->get_field_id('userTwitterID'); ?>">
-					<?php _e('Twitter ID account to send direct message, to find the id, logged into the account you must navigate: Settings, Your data, and there is the id.'); ?> 
-					<input class="widefat" id="<?php echo $this->get_field_id('userTwitterID'); ?>" placeholder="userTwitterID like (942047040904859653)" name="<?php echo $this->get_field_name('userTwitterID'); ?>" type="text" value="<?php echo $userTwitterID; ?>" />
+					<?php _e('Twitter ID account to send direct message, to find the id, logged into the 
+					account you must navigate: Settings, Your data, and there is the id.'); ?> 
+					<input class="widefat" id="<?php echo $this->get_field_id('userTwitterID'); ?>" 
+					placeholder="userTwitterID like (942047040904859653)" name="<?php echo $this->
+					get_field_name('userTwitterID'); ?>" type="text" value="<?php echo $userTwitterID; ?>" 
+					/>
+				</label>
+			</p>
+			<p>
+				<label for="<?php echo $this->get_field_id('message'); ?>">
+					<?php _e('Predefined massage that you send to the previous acoount'); ?> 
+					<input class="widefat" id="<?php echo $this->get_field_id('message'); ?>" 
+					placeholder="message like (942047040904859653)" name="<?php echo $this->
+					get_field_name('message'); ?>" type="text" value="<?php echo $message; ?>" 
+					/>
 				</label>
 			</p>
 		<?php
 
 	}
-}
-	//AUXILIARY METHODS
 	
-	function getRecipientId($instance, $user){
+	public static function getMessageFormateado($instance, $message){
 	
 		$network = ucfirst($socialnetwork);
 	
-		$user = apply_filters('widget_user'.$network, $instance['user'.$network]);
-		$user = str_replace("@", "", $user);
+		$messageFormateado = str_replace(" ","%20",$message);
 	
-	
-		return $user;
+		return $messageFormateado;
 	
 	}
+}
 ?>
