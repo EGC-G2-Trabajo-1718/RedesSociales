d55184781e00e756c1513b0d59309cfd17b75610
ruano23 <carlosruano23@gmail.com>
2018-01-12 16:34:08 +0100

    add: Añadido el widget del timeline por hashtags.
    He terminado de realizar dicho widget y funciona correctamente.
    
    Issue: #29

--- socialhub-egc/widgets/class-timeline-widget.php
+++ socialhub-egc/widgets/class-timeline-widget.php
@@ -21,7 +21,8 @@ class Timeline_Widget extends WP_Widget {
 	}
 
 	/**
-	 * Get the base ID used to identify widgets of this type installed in a widget area
+	 * Get the base ID used to identify widgets of this type installed in a 
+	 widget area
 	 *
 	 * @return string Widget base ID
 	 */
@@ -35,14 +36,17 @@ class Timeline_Widget extends WP_Widget {
 	 * @return string Description of the widget functionality
 	 */
 	public static function getDescription() {
-		return 'This widget implements the timeline for a user social network.';
+		return 'This widget implements the timeline for a user social 
+		network.';
 	}
 
 	/**
 	 * Front-end display of widget
 	 *
-	 * @param array $args     Display arguments including before_title, after_title, before_widget, and after_widget
-	 * @param array $instance The settings for the particular instance of the widget
+	 * @param array $args     Display arguments including before_title, 
+	 after_title, before_widget, and after_widget
+	 * @param array $instance The settings for the particular instance of 
+	 the widget
 	 *
 	 * @return void
 	 */
@@ -52,22 +56,61 @@ class Timeline_Widget extends WP_Widget {
 		
 		//TWITTER
 		$user = $instance['userTwitter'];	
-		if ($user!="")
-		
-		echo '<a class="twitter-timeline" data-lang="en" data-theme="dark" 
-		 data-width="350" data-height="350"
-		data-link-color="#19CF86"
-		href="https://twitter.com/'.$user.'">
-		Tweets by '.$user.'</a>';
+			if ($user!="")		
+				echo '<a class="twitter-timeline"
+						data-lang="en" data-theme="dark" 
+						data-width="350" data-height="350"
+						data-link-color="#19CF86"
+						href="https://twitter.com/'.$user.'">
+						Tweets by '.$user.'</a>
+						</br>
+						</br>';
 		
 		
 		//FACEBOOK - EL USER AQUI ES LA PÁGINA QUE QUERAMOS VISUALIZAR
 		$user = $instance['userFacebook'];	
-		if ($user!="")
-			echo '<div class="fb-page" data-href="https://www.facebook.com/'.$user.'" 
-				data-tabs="timeline" data-small-header="true" data-adapt-container-width="true"
-				data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/'.$user.'"
-				class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/'.$user.'"></a></blockquote></div>';			
+			if ($user!="")
+				echo '<div class="fb-page" data-href="https://www.facebook.com/'.$user.'" 
+						data-tabs="timeline" data-small-header="true" 
+						data-adapt-container-width="true"
+						data-hide-cover="false" 
+						data-show-facepile="false">
+						<blockquote cite="https://www.facebook.com/'.$user.'"
+						class="fb-xfbml-parse-ignore">
+						<a href="https://www.facebook.com/'.$user.'"></a>
+						</blockquote>
+					</div>
+					</br>
+					</br>';
+		
+
+		//TWITTER HASTAG Y ID DEL WIDGET CREADO POR EL USUARIO
+		$hashtag = $instance['hashtag'];	
+		$widgetId = $instance['widgetId'];
+			if ($hashtag!="" && $widgetId!="")		
+				echo '<a class="twitter-timeline"  href="
+						https://twitter.com/hashtag/'.$hashtag.'" 
+						data-widget-id="'.$widgetId.'">
+						Tweets sobre '.$hashtag.'
+					</a>
+									
+					 
+					<script>
+						!function(d,s,id){
+							var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";
+								if(!d.getElementById(id)){
+								js=d.createElement(s);
+								js.id=id;
+								js.src=p+"://platform.twitter.com/widgets.js";
+							fjs.parentNode.insertBefore(js,fjs);
+								}
+						}(document,"script","twitter-wjs");
+					</script>
+					</br>
+					</br>';	
+          
+		                      
+				
 	}
 
 	/**
@@ -83,6 +126,8 @@ class Timeline_Widget extends WP_Widget {
 		$instance = $old_instance;
 		$instance['userTwitter'] = strip_tags($new_instance['userTwitter']);
 		$instance['userFacebook'] = strip_tags($new_instance['userFacebook']);
+		$instance['hashtag'] = strip_tags($new_instance['hashtag']);
+		$instance['widgetId'] = strip_tags($new_instance['widgetId']);
 		$instance = $new_instance;
 
 		return $instance;
@@ -99,22 +144,49 @@ class Timeline_Widget extends WP_Widget {
 		// Output admin widget options form
 		
 		//Getting usernames
-		$userTwitter = esc_attr($instance['userTwitter']);		
+		$userTwitter = esc_attr($instance['userTwitter']);	
+		$userFacebook = esc_attr($instance['userFacebook']);		
+		$hashtag = esc_attr($instance['hashtag']);		
+		$widgetId = esc_attr($instance['widgetId']);		
 		
         ?>
-			<br/><span id="info">Please, input the account whose information will be shown in the timeline</span>
+			<br/><span id="info">Please, input the account whose 
+			information will be shown in the timeline</span>
 			<p>
-				<label for="<?php echo $this->get_field_id('userTwitter'); ?>">
+				<label for="<?php echo $this->get_field_id('userTwitter'); 
+				?>">
 					<?php _e('Twitter account:'); ?> 
-					<input class="widefat" id="<?php echo $this->get_field_id('userTwitter'); ?>" placeholder="TwitterUser" name="<?php echo $this->get_field_name('userTwitter'); ?>" type="text" value="<?php echo $userTwitter; ?>" />
+					<input class="widefat" id="<?php echo $this->
+					get_field_id('userTwitter'); ?>" placeholder="TwitterUser" name="<?php echo $this->get_field_name('userTwitter'); ?>" type="text"
+					value="<?php echo $userTwitter; ?>" />
 				</label>
 			</p>
+			
 			<p>
 				<label for="<?php echo $this->get_field_id('userFacebook'); ?>">
 					<?php _e('Facebook page:'); ?> 
-					<input class="widefat" id="<?php echo $this->get_field_id('userFacebook'); ?>" placeholder="Facebook page" name="<?php echo $this->get_field_name('userFacebook'); ?>" type="text" value="<?php echo $userFacebook; ?>" />
+					<input class="widefat" id="<?php echo $this->
+					get_field_id('userFacebook'); ?>" placeholder="Facebook page" name="<?php echo $this->get_field_name('userFacebook'); ?>" type="text"
+					value="<?php echo $userFacebook; ?>" />
 				</label>
 			</p>
+			
+			<p>
+				<label for="<?php echo $this->get_field_id('hashtag'); ?>">
+					<?php _e('hashtag:'); ?> 
+					<input class="widefat" id="<?php echo $this->
+					get_field_id('hashtag'); ?>" placeholder="hashtag" name="<?php echo $this->get_field_name('hashtag'); ?>" type="text"
+					value="<?php echo $hashtag; ?>" />
+				</label>
+				<span id="info">Please, create your widget in your twitter account and set the "data-widget ID" here.</span>
+				<label for="<?php echo $this->get_field_id('widgetId'); ?>">
+					<?php _e(' You widget ID:'); ?> 
+					<input class="widefat" id="<?php echo $this->
+					get_field_id('widgetId'); ?>" placeholder="widgetId" name="<?php echo $this->get_field_name('widgetId'); ?>" type="text"
+					value="<?php echo $widgetId; ?>" />
+				</label>
+			</p>
+			
 			
 			
 			
