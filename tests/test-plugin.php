1eb9d1bca11101736b4361942168564e6200d388
Dani Martínez <32370013+danmarsua1@users.noreply.github.com>
2018-01-18 11:06:22 +0100

    Update test-plugin.php

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -1,7 +1,15 @@
 <?php
-class Test_Plugin extends WP_UnitTestCase {
+//class Test_Plugin extends WP_UnitTestCase {
+class Test_Plugin extends PHPUnit_Framework_TestCase {
+  
   // Check that that activation doesn't break
   function test_plugin_activated() {
     $this->assertTrue( is_plugin_active( PLUGIN_PATH ) );
   }
+  
+  //Basic Test (TEMPORALY)
+  function neliovat_get_vat( $quantity ) {
+	return $quantity * 0.21;
+  }
+  
 }
