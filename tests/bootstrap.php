98ab05a9595b51b3193baa3a409e309625839692
Dani Martínez <32370013+danmarsua1@users.noreply.github.com>
2018-01-20 15:24:07 +0100

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,8 +21,4 @@ $GLOBALS['wp_tests_options'] = array(
 // If the develop repo location is defined (as WP_DEVELOP_DIR), use that
 // location. Otherwise, we'll just assume that this plugin is installed in a
 // WordPress develop SVN checkout.
-if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
-} else {
-	require '../../../../tests/phpunit/includes/bootstrap.php';
-}
+require '../../../../tests/phpunit/includes/bootstrap.php';
