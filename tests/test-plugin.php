b9c4c0ec581cd72c778a2f255fbd67e4bbdce798
Dani Martínez <32370013+danmarsua1@users.noreply.github.com>
2018-01-20 16:28:46 +0100

    Update test-plugin.php

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -1,25 +1,15 @@
 <?php
 class Test_Plugin extends WP_UnitTestCase {
 //class Test_Plugin extends PHPUnit_Framework_TestCase {
-	
- /**
-  * Run a simple test to ensure that the tests are running
-  */
+
   function test_tests() {
 	  $this->assertTrue( true );
-	  echo "Test are running";
-	  
+	  $this->echo "Test are running";  
   }
-  
-  // Check that that activation doesn't break
+
   function test_plugin_activated() {
 	  $this->assertTrue( is_plugin_active( "socialhub-egc/socialhub-egc.php" ) );
-	  echo "Plugin activated";
-  }
-  
-  //Basic Test (TEMPORALY)
-  function neliovat_get_vat( $quantity ) {
-    return $quantity * 0.21;
+	  $this->echo "Plugin activated";
   }
   
 }
