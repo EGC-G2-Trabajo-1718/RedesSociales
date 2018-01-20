c56e6c4b13e8f3b0dd78362ac3ae1f7dce8858a2
Dani Martínez <32370013+danmarsua1@users.noreply.github.com>
2018-01-20 14:40:59 +0100

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,28 @@
 <?php
 
-$abspath=dirname(getcwd());
+#$abspath=dirname(getcwd());
 
-define('PLUGIN_FILE', getenv('PLUGIN_FILE') );
-$folder = str_replace('.php','', PLUGIN_FILE);
-define('PLUGIN_FOLDER', $abspath.'/tmp/wordpress/wp-content/plugins/'.$folder);
-define('PLUGIN_PATH', PLUGIN_FOLDER.'/'.PLUGIN_FILE);
+#define('PLUGIN_FILE', getenv('PLUGIN_FILE') );
+#$folder = str_replace('.php','', PLUGIN_FILE);
+#define('PLUGIN_FOLDER', $abspath.'/tmp/wordpress/wp-content/plugins/'.$folder);
+#define('PLUGIN_PATH', PLUGIN_FOLDER.'/'.PLUGIN_FILE);
 
 // Activates this plugin in WordPress
-$GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array(PLUGIN_PATH),
-);
+#$GLOBALS['wp_tests_options'] = array(
+	#'active_plugins' => array(PLUGIN_PATH),
+#);
 	
-require $abspath.'/tmp/wordpress/tests/phpunit/includes/bootstrap.php';
+#require $abspath.'/tmp/wordpress/tests/phpunit/includes/bootstrap.php';
+
+// Activates this plugin in WordPress so it can be tested.
+$GLOBALS['wp_tests_options'] = array(
+	'active_plugins' => array( 'socialhub-egc/socialhub-egc.php' ),
+);
+// If the develop repo location is defined (as WP_DEVELOP_DIR), use that
+// location. Otherwise, we'll just assume that this plugin is installed in a
+// WordPress develop SVN checkout.
+if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
+} else {
+	require '../../../../tests/phpunit/includes/bootstrap.php';
+}
