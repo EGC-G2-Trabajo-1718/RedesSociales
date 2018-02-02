993b7e550766558d2dd0748eeef9a6dc78ad8ab5
Dani Martínez <32370013+danmarsua1@users.noreply.github.com>
2018-02-02 12:06:15 +0100

    Update test-follow-button-widget.php

--- tests/test-follow-button-widget.php
+++ tests/test-follow-button-widget.php
@@ -15,7 +15,7 @@ class Test_Follow_Button_Widget extends WP_UnitTestCase {
 	
 	//Test that Follow_Button_Widget is registered in Wordpress as Widget
 	function test_register_widget_core_widget() {
-		$dir = dirname(__FILE__). '../socialhub-egc/widgets/class-follow-button-widget.php';
+		$dir = '../socialhub-egc/widgets/class-follow-button-widget.php';
 		require $dir;
 		$widget = new Follow_Button_Widget();
 		//$this->assertEquals( '..socialhub-egc/widgets/class-follow-button-widget', $widget->widget_options['classname'] );
