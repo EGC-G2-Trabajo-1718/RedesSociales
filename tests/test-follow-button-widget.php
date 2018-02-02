59e183431ecc06fb565f1ecbafd5858aac3c06f4
Dani Martínez <32370013+danmarsua1@users.noreply.github.com>
2018-02-02 09:37:21 +0100

    Update test-follow-button-widget.php

--- tests/test-follow-button-widget.php
+++ tests/test-follow-button-widget.php
@@ -16,12 +16,9 @@ class Test_Follow_Button_Widget extends WP_UnitTestCase {
 	//Test that Follow_Button_Widget is registered in Wordpress as Widget
 	function test_register_widget_core_widget() {
 		global $wp_widget_factory;
-		//unregister_widget( '../wp-content/plugins/socialhub-egc/widgets/class-follow-button-widget.php' );
-		//register_widget( '../wp-content/plugins/socialhub-egc/widgets/class-follow-button-widget.php' );
-		unregister_widget( 'socialhub-egc/widgets/class-follow-button-widget.php' );
-		register_widget( 'socialhub-egc/widgets/class-follow-button-widget.php' );
-		//$this->assertTrue( isset( $wp_widget_factory->widgets['../wp-content/plugins/socialhub-egc/widgets/class-follow-button-widget.php'] ) );
-		$this->assertTrue( isset( $wp_widget_factory->widgets['socialhub-egc/widgets/class-follow-button-widget.php'] ) );
+		unregister_widget( '../wp-content/plugins/socialhub-egc/widgets/class-follow-button-widget.php' );
+		register_widget( '../wp-content/plugins/socialhub-egc/widgets/class-follow-button-widget.php' );
+		$this->assertTrue( isset( $wp_widget_factory->widgets['../wp-content/plugins/socialhub-egc/widgets/class-follow-button-widget.php'] ) );
 	}
   
 }
