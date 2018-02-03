2ec6931e2c99c6819f4e27efff12defd05d5a8a0
Juan Huerta <32852855+juahueceb1@users.noreply.github.com>
2018-02-03 23:13:55 +0100

    fix: parámetro en método test_button
    El test no se ejecutaba correctamente porque el intérprete de PHP interpretaba el parámetro de la función formatTwitterUsername como una variable porque contenía el carácter $. Para solucionarlo, se ha cambiado este carácter para que el intérprete reconozca el parámetro como un string.

--- tests/test-share-button-widget.php
+++ tests/test-share-button-widget.php
@@ -3,8 +3,8 @@ include_once("../../../socialhub-egc/widgets/class-share-button-widget.php");
 class Test_Share_Button_Widget extends WP_UnitTestCase {
 	
 	function test_button() {
-		$username = Share_Button_Widget::formatTwitterUsername("$test 9");
-		$this->assertEquals($username, 'test 9', 'Error in the method formatTwitterUsername');
+		$username = Share_Button_Widget::formatTwitterUsername('(test 9');
+		$this->assertEquals($username, 'test9', 'Error in the method formatTwitterUsername');
 	}
 }
 ?>
