228aaae16b3ed98ff487341f4eac2ad8821ad4f8
Dani Martínez <32370013+danmarsua1@users.noreply.github.com>
2018-02-03 14:24:44 +0100

    Update test-follow-button-widget.php

--- tests/test-follow-button-widget.php
+++ tests/test-follow-button-widget.php
@@ -10,7 +10,7 @@ class Test_Follow_Button_Widget extends WP_UnitTestCase {
 	
 	//Test that the wordpress plugin is activated
 	function test_plugin_activated() {
-	  	$this->assertTrue( is_plugin_active( "socialhub-egc/socialhub-egc.php" ) ,"Plugin activado");
+	  	$this->assertTrue(is_plugin_active( "socialhub-egc/socialhub-egc.php" ));
 	  	return "Plugin activated";
   	}
 	
