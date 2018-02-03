3012b3b0a9560af927e530332655687c0ad23d6c
Dani Martínez <32370013+danmarsua1@users.noreply.github.com>
2018-02-03 13:52:31 +0100

    Update test-follow-button-widget.php

--- tests/test-follow-button-widget.php
+++ tests/test-follow-button-widget.php
@@ -4,7 +4,7 @@ class Test_Follow_Button_Widget extends WP_UnitTestCase {
 	// Check that the plugin is active
 	function test_plugin_active() {
 		// WP_Error on invalid file or 'NULL' on success 
-		$result = activate_plugin('socialhub-egc/socialhub-egc.php');
+		$result = activate_plugin('../../../socialhub-egc/socialhub-egc.php');
 		$this->assertNull($result);
 
 		return "Plugin active!";
