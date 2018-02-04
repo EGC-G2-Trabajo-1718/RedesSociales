bc34558e9d43c119d0bfe41f08ece3b874ca6a44
Juan H <juahueceb1@alum.us.es>
2018-02-04 19:32:24 +0100

    add: pruebas para función 'formatTwitterUsername'
    Se han añadido dos tests que comprueban que la función 'formatTwitterUsername' devuelve un username con el formato correcto (letras, números y guiones bajos).
    
    Issue #45

--- tests/test-share-button-widget.php
+++ tests/test-share-button-widget.php
@@ -10,11 +10,23 @@ class Test_Share_Button_Widget extends WP_UnitTestCase {
 	
 	// Test 1: Check that the widget is correctly registered in WordPress
 	function test_1() {
-		echo "--------------------------";
-		echo " TEST SHARE BUTTON WIDGET ";
-		echo "--------------------------";
-		echo "Test 1... ";
 		$this->assertTrue(is_active_widget('Share_Button_Widget'), 'Widget is not active!');
+	}
+
+	// Test 2: Check that Twitter username is not allowed with spaces, 
+	//		   so formatTwitterUsername fixes this error
+	function test_2() {
+		$username = Share_Button_Widget::formatTwitterUsername(' test  1 ');
+		$this->assertEquals($username, 'test1', 
+			'Error in the function formatTwitterUsername: username with spaces');
+	}
+
+	// Test 3: Check that Twitter username can only contain letters, numbers and underscores, 
+	//         so formatTwitterUsername fixes characters not allowed
+	function test_2() {
+		$username = Share_Button_Widget::formatTwitterUsername('()te&st_2?');
+		$this->assertEquals($username, 'test_2', 
+			'Error in the function formatTwitterUsername: username with characters not allowed');
 	}
 }
 ?>
