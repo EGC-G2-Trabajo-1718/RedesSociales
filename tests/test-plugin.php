5328e415c7657267ed8951e308d35a19b59aac56
Dani Martínez <32370013+danmarsua1@users.noreply.github.com>
2018-02-04 13:23:43 +0100

    Create test-plugin.php

---
+++ tests/test-plugin.php
@@ -0,0 +1,11 @@
+//Tests is running
+	function test_tests() {
+		$this->assertTrue( true );
+		return "Tests are running";  
+	}
+	
+	//Test that the wordpress plugin is activated
+	function test_plugin_activated() {
+	  	$this->assertTrue(is_plugin_active( "socialhub-egc/socialhub-egc.php" ),"Plugin is not active");
+	  	return "Plugin activated";
+  }
