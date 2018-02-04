9fbe028d484632e80bb0e2c553d409b036079975
Juan H <juahueceb1@alum.us.es>
2018-02-04 20:36:04 +0100

    add: pruebas para función 'getTweetParameters'
    Se han añadido cuatro tests que comprueban que la función 'getTweetParameters' genera correctamente los parámetros (text, hashtags y via) de una URL con diferentes casuísticas.
    
    Issue #45

--- tests/test-share-button-widget.php
+++ tests/test-share-button-widget.php
@@ -23,10 +23,46 @@ class Test_Share_Button_Widget extends WP_UnitTestCase {
 
 	// Test 3: Check that Twitter username can only contain letters, numbers and underscores, 
 	//         so formatTwitterUsername fixes characters not allowed
-	function test_2() {
+	function test_3() {
 		$username = Share_Button_Widget::formatTwitterUsername('()te&st_2?');
 		$this->assertEquals($username, 'test_2', 
 			'Error in the function formatTwitterUsername: username with characters not allowed');
+	}
+
+	// Test 4: Check a tweet with only text
+	// 		   getTweetParameters($tweetText, $hashtags, $sourceTweet)
+	function test_4() {
+		$tweetParameters = Share_Button_Widget::getTweetParameters('Test','','');
+		$this->assertEquals($tweetParameters, '?text=Test', 
+			'Error in the function getTweetParameters: check a tweet with only text');
+	}
+
+	// Test 5: Check a tweet with only hashtags
+	function test_5() {
+		$tweetParameters = Share_Button_Widget::getTweetParameters('','Test','');
+		$this->assertEquals($tweetParameters, '?hashtags=Test', 
+			'Error in the function getTweetParameters: check a tweet with only hashtags');
+	}
+
+	// Test 6: Check a tweet with text and hashtags
+	function test_6() {
+		$tweetParameters = Share_Button_Widget::getTweetParameters('Test','Test','');
+		$this->assertEquals($tweetParameters, '?text=Test&hashtags=Test', 
+			'Error in the function getTweetParameters: check a tweet with text and hashtags');
+	}
+
+	// Test 7: Check a tweet with only username
+	function test_7() {
+		$tweetParameters = Share_Button_Widget::getTweetParameters('','','Test');
+		$this->assertEquals($tweetParameters, '?via=Test', 
+			'Error in the function getTweetParameters: check a tweet with only username');
+	}
+
+	// Test 8: Check a tweet with text, hashtags and username
+	function test_8() {
+		$tweetParameters = Share_Button_Widget::getTweetParameters('Test','Test','Test');
+		$this->assertEquals($tweetParameters, '?text=Test&hashtags=Test&via=Test', 
+			'Error in the function getTweetParameters: check a tweet with text, hashtags and username');
 	}
 }
 ?>
