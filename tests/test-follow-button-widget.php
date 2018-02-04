a4985f6c23470a69fb6a83bfb7a78aefaeb6b191
Dani Martínez <32370013+danmarsua1@users.noreply.github.com>
2018-02-04 13:23:33 +0100

    Update test-follow-button-widget.php

--- tests/test-follow-button-widget.php
+++ tests/test-follow-button-widget.php
@@ -1,18 +1,6 @@
 <?php
 class Test_Follow_Button_Widget extends WP_UnitTestCase {
 	
-	//Tests is running
-	function test_tests() {
-		$this->assertTrue( true );
-		return "Test are running";  
-	}
-	
-	//Test that the wordpress plugin is activated
-	function test_plugin_activated() {
-	  	$this->assertTrue(is_plugin_active( "socialhub-egc/socialhub-egc.php" ),"Plugin is not active");
-	  	return "Plugin activated";
-  	}
-	
 	//Test that Follow_Button_Widget is registered in Wordpress as Widget
 	function test_register_widget_core_widget() {
 		global $wp_widget_factory;
