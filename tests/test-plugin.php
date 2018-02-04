14bc658137a10bf743a9158d38051fb3b81fdc40
Juan Huerta <32852855+juahueceb1@users.noreply.github.com>
2018-02-04 21:11:09 +0100

    fix: comentarios del test y código
    Se han corregido los comentarios del test y se han quitado dos líneas de código erróneas.

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -1,17 +1,21 @@
 <?php
-
+/**
+ * Test_Plugin
+ *
+ * Test that plugin is active.
+ *
+ * @see WP_UnitTestCase
+ */
 class Test_Plugin extends WP_UnitTestCase {	
 
-	//Tests is running
-	function test_tests() {
-		$this->assertTrue( true );
-		return "Tests are running";  
+	// Test that PHPUnit works
+	function test_1() {
+		$this->assertTrue(true); 
 	}
 	
-	//Test that the wordpress plugin is activated
-	function test_plugin_activated() {
-	  	$this->assertTrue(is_plugin_active( "socialhub-egc/socialhub-egc.php" ),"Plugin is not active");
-	  	return "Plugin activated";
+	// Test that the WordPress plugin is active
+	function test_2() {
+	  	$this->assertTrue(is_plugin_active('socialhub-egc/socialhub-egc.php'), 'Plugin is not active!');	  	
   	}
-
 }
+?>
