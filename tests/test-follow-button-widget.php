77889f315ec6be2ae487a9eb30df1e5e9c970de7
danmarsua1 <djmartinezsuarez91@gmail.com>
2018-02-04 14:24:46 +0100

    fix: Se ha modificado la ruta del método include de php
    Se ha modificado la ruta relativa del método include de php, ya que,
    además de que hay que ponerlo antes de crear  la clase, solo se incluye
    una vez en el fichero php.
    
    Issue #44

--- tests/test-follow-button-widget.php
+++ tests/test-follow-button-widget.php
@@ -1,31 +1,36 @@
 <?php
+include_once '../../../socialhub-egc/widgets/class-follow-button-widget.php';
 class Test_Follow_Button_Widget extends WP_UnitTestCase {
 	
 	//Test that Follow_Button_Widget is registered in Wordpress as Widget
 	function test_register_widget_core_widget() {
+
 		global $wp_widget_factory;
 		$this->assertTrue( isset( $wp_widget_factory->widgets['Follow_Button_Widget'] ) );
+
 	}
 
 	//Test that Follow_Button_Widget is able to create
 	function test_create_widget() {
+
 		include_once '../socialhub-egc/widgets/class-follow-button-widget.php';
 		$widget = new Follow_Button_Widget();
+
 	}
 
 	//Test that probe the construct run correctly for this widget
 	function test_wp_widget_constructor() {
-		include_once '../socialhub-egc/widgets/class-follow-button-widget.php';
 
 		$name = 'Follow Buttons by EGC';
 		$widget = new Follow_Button_Widget();
 
 		$this->assertEquals( $name, $widget->name );
+
 	}
 
 	//Check that the twitter user's characters are valid
 	function test_wp_widget_twitter_format() {
-		include_once '../socialhub-egc/widgets/class-follow-button-widget.php';
+
 		$widget = new Follow_Button_Widget();
 
 		$instance = [
@@ -36,6 +41,7 @@ class Test_Follow_Button_Widget extends WP_UnitTestCase {
 		$username = $widget->getUsername($instance, $socialnetwork);
 
 		$this->assertEquals( $username, "test", 'Error in the method "getUsername"' );
+
 	}
 	
   
