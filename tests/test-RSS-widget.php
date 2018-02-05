23d705c093c508640881e5103e1a720fa76d2d66
Alberto Gómez <albgomceb@alum.us.es>
2018-02-05 15:53:33 +0100

    fix: Actualización de los test
    He realizado una actualización de los test. Realizo el commit para
    probar si se adapta adecuadamente a Travis CI.
    
    Issue: #49

--- tests/test-RSS-widget.php
+++ tests/test-RSS-widget.php
@@ -6,27 +6,29 @@
  *
  * @see WP_UnitTestCase
  */
-class Test_RSS extends WP_UnitTestCase {	
+class Test_RSS extends WP_UnitTestCase {
 
-	//Test that Follow_Button_Widget is registered in Wordpress as Widget
+	//Test RSS_Widget is registered in Wordpress as Widget
 	function test_register_widget_rss() {
 		global $wp_widget_factory;
 		$this->assertTrue( isset( $wp_widget_factory->widgets['RSS_Widget'] ) );
 	}
 	
-	//Test that the WordPress plugin is active
-	function test_2() {
-	  	$this->assertTrue(is_plugin_active('socialhub-egc/socialhub-egc.php'), 'Plugin is not active!');	  	
-  	}
-
-  	//Test that probe the construct run correctly for this widget
+  	//Test that probe the button RSS work correctly.
  	function test_wp_widget_constructor() {
  
- 		$name = 'RSS, Feedly and Flipboard buttons by EGC';
- 		$widget = new RSS_Widget();
- 
- 		$this->assertEquals( $name, $widget->name );
+ 		$rss_widget = new RSS_Widget();
+ 		$instance = [
+    			"rss" => "yes",
+		];
+
+		$args = NULL;
+
+		//Comparar que la salida de widget contiene rss
+		$widget = $rss_widget->widget($args, $instance)
+		$this->assertTrue(strpos($widget, 'egc-rss-container'));
  
  	}
+
 }
 ?>
